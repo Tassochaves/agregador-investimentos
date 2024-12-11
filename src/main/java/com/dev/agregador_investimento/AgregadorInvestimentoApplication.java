50cdadd682c7c67e9d0fda774b061d760595e397
Tassochaves <tassogt@hotmail.com>
2024-12-10 22:24:12 -0300

    First commit

---
+++ src/main/java/com/dev/agregador_investimento/AgregadorInvestimentoApplication.java
@@ -0,0 +1,13 @@
+package com.dev.agregador_investimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgregadorInvestimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgregadorInvestimentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/agregador_investimento/AgregadorInvestimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.agregador_investimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgregadorInvestimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
