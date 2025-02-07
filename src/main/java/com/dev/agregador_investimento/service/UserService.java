53120cb387f667d1984c172c0fb830e2dedafe02
Tassochaves <tassogt@hotmail.com>
2025-02-07 15:45:42 -0300

    Feat: Implements user search by id

--- src/main/java/com/dev/agregador_investimento/service/UserService.java
+++ src/main/java/com/dev/agregador_investimento/service/UserService.java
@@ -1,6 +1,7 @@
 package com.dev.agregador_investimento.service;
 
 import java.time.Instant;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
@@ -31,5 +32,9 @@ public class UserService {
         var userSaved = userRepository.save(entity);
 
         return userSaved.getUserId();
+    }
+
+    public Optional<User> getUserById(String userId) {
+        return userRepository.findById(UUID.fromString(userId));
     }
 }
